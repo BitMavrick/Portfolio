5ac3a3571054ca69ec31a8d0b5c371875dba2c3a
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-03-02 11:16:10 +0600

    installing fresh laravel

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\generalController;
 
-Route::get('/msgs', [generalController::class, 'messages'])->name('messages');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
